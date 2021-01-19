a78f0fe825bfd40b93d3c1bf5c61d2ae99a56a1e
Morgan Bradford <morganjbradford95@gmail.com>
2021-01-19 17:19:27 -0600

    add forEachLoop
    Co-authored-by: Theron Packus <tlpackus@gmail.com>
    Co-authored-by: DeAunte Hall <godsofolympus88@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+    event.preventDefault()
     const person1Input = $("input#noun1").val();
     const person2Input = $("input#pastTenseVerb1").val();
     const person3Input = $("input#adverb1").val();
@@ -14,10 +15,11 @@ $(document).ready(function() {
 
     const responseArray = [["#userNoun1", person1Input], ["#userPastTenseVerb1", person2Input], ["#userAdverb1", person3Input], ["#userAdjective1", person4Input], ["#userNoun2", person5Input], ["#userNoun3", person6Input], ["#userAdjective2", person7Input], ["#userVerb1", person8Input], ["#userAdverb2", person9Input], ["#userPastTenseVerb2", person10Input], ["#userAdjective3", person11Input]];
 
-   
+    responseArray.forEach(function(responsePair) {
+      $(responsePair[0]).text(responsePair[1]);
+    });
+    
 
     $("#story").show();
-
-    event.preventDefault()
   });
 });
