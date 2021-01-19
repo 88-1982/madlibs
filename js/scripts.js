4eb9906f3efbb3f8244ce2a42bc11c2947937f31
Morgan Bradford <morganjbradford95@gmail.com>
2021-01-19 17:17:59 -0600

    add responseArray
    Co-authored-by: Theron Packus <tlpackus@gmail.com>
    Co-authored-by: DeAunte Hall <godsofolympus88@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,9 @@ $(document).ready(function() {
     const person10Input = $("input#pastTenseVerb2").val();
     const person11Input = $("input#adjective3").val();
 
-   
+    const responseArray = [["#userNoun1", person1Input], ["#userPastTenseVerb1", person2Input], ["#userAdverb1", person3Input], ["#userAdjective1", person4Input], ["#userNoun2", person5Input], ["#userNoun3", person6Input], ["#userAdjective2", person7Input], ["#userVerb1", person8Input], ["#userAdverb2", person9Input], ["#userPastTenseVerb2", person10Input], ["#userAdjective3", person11Input]];
 
+   
 
     $("#story").show();
 
